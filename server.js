942887433f18844c71dd88434321f0e30aa146e2
Babken Bayadyan <babkenbv@gmail.com>
2021-02-05 13:50:17 +0400

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin, 
+  getCurrentUser, 
+  userLeaves, 
+  getRoomUsers
+} = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botname = 'ChatBot';
+
+io.on('connection', socket => {
+
+  socket.on('joinRoom', ({username, room}) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    socket.emit('message', formatMessage(botname, 'Welcome to Chatable'))
+
+    socket.broadcast.to(user.room).emit('message', formatMessage(botname, `${user.username} has joined the chat`));
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      })
+  });
+
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  socket.on('disconnect', () => {
+    const user = userLeaves(socket.id);
+    if(user) {
+     io.to(user.room).emit('message', formatMessage(botname, `${user.username} has left the chat`));
+     
+     io.to(user.room).emit('roomUsers', {
+       room: user.room,
+       users: getRoomUsers(user.room)
+     })
+    }
+  });
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
